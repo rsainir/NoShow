048436374c2f294803ea292d3a95ca8951af06c4
rsainir <44027114+rsainir@users.noreply.github.com>
2020-03-31 12:11:52 -0400

    combine intake form + frontend

---
+++ Client/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from Client.models import clientIntake
+
+class ClientRegisterForm(UserCreationForm):
+	email = forms.EmailField(max_length=200, help_text='Required')
+
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
+
+class ClientIntakeForm(forms.ModelForm):
+	class Meta:
+		model = clientIntake
+		fields = ['firstName','lastName','streetAddress','city','zipCode','number','employerName','advice', 'partiesInvolved','desiredOutcome','acceptOutcome','author']
---
+++ Client/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+	url(r'^$', views.client_page, name='client-page'),
+	url(r'^register/$', views.registration_page, name='registration-page'),
+	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    	views.activate, name='activate'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'Client/client_login.html'), name='login-page'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'Client/client_logout.html'), name='logout-page'),
+    path('profile/', views.client_profile_page, name='client-profile-page'),
+    path('intakeForm/', views.client_intake_page,name='client-intake-page'),
+]
