18a2379f9cd22b69816e7c3d1f73185350bc7508
Raul Salazar <raulsalazarpacheco@gmail.com>
2020-04-15 15:42:56 -0400

    email confirmation and forgot password are completed

--- G_Law/urls.py
+++ G_Law/urls.py
@@ -20,10 +20,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('client/', include('Client.urls')),
     url(r'^$', views.test_redirect1, name='test_redirect1'),
     url(r'^$', views.test_redirect2, name='test_redirect2'),
+    
+
 
 ]
